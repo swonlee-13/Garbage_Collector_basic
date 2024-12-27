e6719e41b475407ec9734dea26db0355ffacbd30
gbssw13 <gbssw13@gmail.com>
2024-12-28 07:03:07 +0900

    miao

--- main.cpp
+++ main.cpp
@@ -8,13 +8,16 @@
 using namespace std;
 
 // 시간을 기록해놓기 위해서 주소, 사이즈, 생성시간을 들고 저장.
-struct MemoryBlock {
+struct MemoryBlock
+{
   void *address;
   size_t size;
   chrono::system_clock::time_point allocationTime;
+  bool isFreed = false; // 해제 상태 추적
 };
 
-class GarbageCollector {
+class GarbageCollector
+{
 private:
   // 싱글톤 객체
   static GarbageCollector *instance;
@@ -30,14 +33,17 @@ private:
   // 싱글톤을 위한 변수
   bool running = true;
 
-  // 생성자가 발동되면서, period 는 500ms으로 잡고 시작하고,
+  // 생성자가 발동되면서, period 는 900ms으로 잡고 시작하고,
   // 가비지 콜렉터 모니터링 스레드를 돌립니다.
-  GarbageCollector() : period(500) {
+  GarbageCollector() : period(900)
+  {
     th = thread(&GarbageCollector::timer, this, period);
   }
   // 이게 모니터링 쓰레드.
-  void timer(int period) {
-    while (running) {
+  void timer(int period)
+  {
+    while (running)
+    {
       this_thread::sleep_for(chrono::milliseconds(period));
       collectGarbage();
     }
@@ -45,7 +51,8 @@ private:
 
   // 메모리블록을 벡터에 추가하는 함수.
   void addMemoryBlock(void *address, size_t size, bool isReallocation,
-                      void *prevAddr = nullptr) {
+                      void *prevAddr = nullptr)
+  {
     if (!address)
       return;
     lock_guard<recursive_mutex> lock(mutex);
@@ -54,24 +61,26 @@ private:
   }
 
   // 시간을 확인하면서 1초 이상이면, 할당해제.
-  void collectGarbage() {
+  void collectGarbage()
+  {
     lock_guard<recursive_mutex> lock(mutex);
     auto now = chrono::system_clock::now();
     vector<MemoryBlock> newBlocks;
+    bool collected = false;
 
-    for (const auto &block : memoryBlocks) {
-      auto duration =
-          chrono::duration_cast<chrono::seconds>(now - block.allocationTime)
-              .count();
-      if (duration >= 1) {
-        if (block.address != nullptr) { // nullptr 체크
-          try {
-            free(block.address);
-          } catch (...) {
-            cout << "Failed to free memory at " << block.address << endl;
-          }
-        }
-      } else {
+    for (const auto &block : memoryBlocks)
+    {
+      if (block.isFreed)
+        continue; // 이미 해제된 블록은 스킵
+      auto duration = chrono::duration_cast<chrono::seconds>(now - block.allocationTime).count();
+
+      if (duration >= 5)
+      {
+        collected = true;
+        free(block.address);
+      }
+      else
+      {
         newBlocks.push_back(block);
       }
     }
@@ -82,18 +91,22 @@ private:
 
 public:
   // 싱글톤은 이렇게 콜 해서 사용합니다.
-  static GarbageCollector &GetInstance() {
+  static GarbageCollector &GetInstance()
+  {
     lock_guard<recursive_mutex> lock(mutex);
-    if (instance == nullptr) {
+    if (instance == nullptr)
+    {
       instance = new GarbageCollector();
     }
     return *instance;
   }
 
   // 현재 할당여부를 확인. 의도하지 않은 동작 방어
-  bool checkExistingAllocation(void *address) {
+  bool checkExistingAllocation(void *address)
+  {
     lock_guard<recursive_mutex> lock(mutex);
-    for (const auto &block : memoryBlocks) {
+    for (const auto &block : memoryBlocks)
+    {
       if (block.address == address)
         return true;
     }
@@ -101,7 +114,8 @@ public:
   }
 
   // 오브젝트를 생성할 때 뮤텍스를 걸어주고 해야해서 한번 감싸준 함수.
-  void createObject(void *address, size_t size) {
+  void createObject(void *address, size_t size)
+  {
     lock_guard<recursive_mutex> lock(mutex);
     addMemoryBlock(address, size, false, nullptr);
   }
@@ -109,17 +123,20 @@ public:
   // 직접 가비지 콜렉터 호출할 떄 씁니다.
   // 해당 함수는 기능상 private 에 놨기 때문에 이렇게 퍼블릭 래핑 함수가 필요.
   //  출력문은 지우고 싶으면 지우세요.
-  void manualGarbage() {
+  void manualGarbage()
+  {
     cout << "manual GC" << endl;
     GarbageCollector::GetInstance().collectGarbage();
   }
 
   // 출력함수입니다. 원래 함수 안에서 막 출력했는데, 불편해서 기능을
   // 분리했습니다.
-  void printAddressList() {
+  void printAddressList()
+  {
     cout << "garage collected." << endl;
     cout << "[";
-    for (const auto &block : memoryBlocks) {
+    for (const auto &block : memoryBlocks)
+    {
       cout << block.address << " ";
     }
     cout << "]" << endl;
@@ -128,17 +145,24 @@ public:
 
   // 소명자입니다. 소멸할 때 쓰레드 회수는 해야하고
   // 메모리블록을 순회하면서 다 지우고 소멸하게 했습니다.
-  ~GarbageCollector() {
+  ~GarbageCollector()
+  {
     running = false;
-    if (th.joinable()) {
+    if (th.joinable())
+    {
       th.join();
     }
 
-    for (const auto &block : memoryBlocks) {
-      if (block.address != nullptr) {
-        try {
+    for (const auto &block : memoryBlocks)
+    {
+      if (block.address != nullptr)
+      {
+        try
+        {
           free(block.address);
-        } catch (...) {
+        }
+        catch (...)
+        {
           cout << "Failed to free memory at " << block.address << endl;
         }
       }
@@ -157,12 +181,15 @@ public:
 GarbageCollector *GarbageCollector::instance = nullptr;
 recursive_mutex GarbageCollector::mutex;
 
-struct member {
+struct member
+{
   int x;
   int y;
 };
 
-template <typename T> class smartptr {
+template <typename T>
+class smartptr
+{
 private:
   T *ptr;
 
@@ -174,7 +201,8 @@ public:
   ~smartptr() {}
 
   // 가비지콜렉터가 지워줄꺼라 재할당시 delete 코드는 뺐습니다.
-  smartptr &operator=(T *p) {
+  smartptr &operator=(T *p)
+  {
     ptr = p;
     return *this;
   }
@@ -184,13 +212,16 @@ public:
   smartptr(const smartptr &) = delete;
   smartptr &operator=(const smartptr &) = delete;
 
-  smartptr(smartptr &&other) noexcept {
+  smartptr(smartptr &&other) noexcept
+  {
     ptr = other.ptr;
     other.ptr = nullptr;
   }
 
-  smartptr &operator=(smartptr &&other) noexcept {
-    if (this != &other) {
+  smartptr &operator=(smartptr &&other) noexcept
+  {
+    if (this != &other)
+    {
       delete ptr;
       ptr = other.ptr;
       other.ptr = nullptr;
@@ -205,7 +236,8 @@ public:
 //  그렇지 않으면 그냥 무한루프에 걸려요. isRecursive 를 주석걸고 해보시면
 //  압니다. 그래서 처음에 1회 발동하는 new 는 그냥 할당만, 그 다음부터는 GC에
 //  넣는 추가 기능이 있는 new 로 동작하게 만들었습니다.
-void *operator new(size_t size) {
+void *operator new(size_t size)
+{
   static thread_local bool isRecursive = false;
   if (isRecursive)
     return malloc(size);
@@ -223,7 +255,8 @@ void *operator new(size_t size) {
   return ptr;
 }
 
-int main() {
+int main()
+{
   cout << "action start." << endl;
 
   int *ptr;
@@ -256,7 +289,8 @@ int main() {
   // 메뉴얼갈비지
   GarbageCollector::GetInstance().manualGarbage();
 
-  while (1) {
+  while (1)
+  {
     ptr = new int;
     qtr = new member;
     rtr = new member;
